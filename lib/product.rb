558e45048d2f0133168fd91dd3c049de6695be2a
marlenaonrails <marlenabowen@gmail.com>
2016-03-01 22:07:15 +0000

    completed product file per requirements in app.rb

--- lib/product.rb
+++ lib/product.rb
@@ -1,12 +1,40 @@
 class Product 
+    attr_reader :title, :price, :stock
+    
     @@products = []
     
     def initialize(options={})
-        @@products << self 
+        @title = options[:title]
+        @price = options[:price]
+        @stock = options[:stock]
+        add_to_products
     end
     
     def self.all
         @@products
     end
+    
+    def self.in_stock
+        @@products.select {|p| p.in_stock?} 
+    end
+    
+    def in_stock?
+        @stock > 0 
+    end
+    
+    private 
+    
+    def self.find_by_title(title)
+        @@products.find {|p| p.title == title} 
+    end
+    
+    def add_to_products
+        product = self.class.find_by_title(@title)
+        if product
+            raise DuplicateProductError, "This product has already been entered"
+        else
+            @@products << self 
+        end
+    end
+    
 end
-
