c1ba867530b3397ec58cf8c3a16259d3cb24cda0
marlenaonrails <marlenabowen@gmail.com>
2016-03-11 15:49:32 +0000

    finished transactions section, added new list method feature for customer and product, trying to add total_stock_value method feature

--- app.rb
+++ app.rb
@@ -46,23 +46,44 @@ puts walter.name # Should return "Walter Latimer"
 
 # TRANSACTIONS
 
-# transaction = Transaction.new(walter, nanoblock)
+transaction = Transaction.new(walter, nanoblock)
 
-# puts transaction.id # Should return 1
-# puts transaction.product == nanoblock # Should return true
-# puts transaction.product == firehouse # Should return false
-# puts transaction.customer == walter # Should return true
-
-# puts nanoblock.stock # Should return 11
+puts transaction.id # Should return 1
+puts transaction.product == nanoblock # Should return true
+puts transaction.product == firehouse # Should return false
+puts transaction.customer == walter # Should return true
+puts nanoblock.stock # Should return 11
 
 # PURCHASES
 
-# puts walter.purchase(nanoblock)
+puts walter.purchase(nanoblock) 
 
-# puts Transaction.all.count # Should return 2
+puts Transaction.all.count # Should return 2
 
-# transaction2 = Transaction.find(2)
-# puts transaction2.product == nanoblock # Should return true
+transaction2 = Transaction.find(2)
+puts transaction2.product == nanoblock # Should return true
 
 # walter.purchase(firehouse)
 # Should return OutOfStockError: 'LEGO Firehouse Headquarter' is out of stock.
+
+#NEW FEATURES 
+puts "++++++++++++++++++++++++++++++++++++++++++++++"
+#prints each product title in alphabetical order 
+puts "Products: "
+Product.list.each do |product|
+    puts product.title
+end
+#prints each customer name in alphabetical order 
+puts "Customers: "
+Customer.list.each do |customer| 
+    puts customer.name 
+end 
+
+puts Product.total_stock_value
+
+
+
+
+
+
+
